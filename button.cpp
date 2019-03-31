ec959fb604b74bea327ebe6d8361392886beb3b5
njacks1 <45337815+njacks1@users.noreply.github.com>
2019-03-31 14:03:46 -0400

    Update button.cpp

--- button.cpp
+++ button.cpp
@@ -1,10 +1,24 @@
 #include "library.h"
 
-void main(){
-int x = 200;
-int y = 200;
-int radius = 100;
-double const pi = acos(-1.0);
-make_window(400, 400);
-button(x,y,radius,pi);
+void drawButton(const int x, const int y, const int radius, const char c, const int fontSize, const int color){
+	set_heading_degrees(0);
+	move_to(x, y);
+	set_pen_width(radius);
+	set_pen_color(color);
+	draw_point();
+	set_font_size(fontSize);
+	set_pen_color(color::black);
+	write_char(c, direction::center);
 }
+
+void button(){
+	drawButton(x, y, radius, c, fontSize, color);
+}
+
+void main(){
+	make_window(200, 200);
+	drawButton(100, 100, 100, '@', 20, color::blue);
+}
+
+const char calculatorSymbols[][5] = { { 7, 8, 9, +, L'\u263C' }, { 4, 5, 6, -, L'\u2191' }, { 1, 2, 3, x, l }, { C, 0, = , L'÷', % } }
+
